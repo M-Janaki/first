6d452a9ee09db97fb425ecc583069ee4c701460e
M-Janaki <janaki10nandhu@gmail.com>
2023-07-11 12:44:35 +0530

    commited

---
+++ IfElse.java
@@ -0,0 +1,80 @@
+
+package pkgif.pkgelse;
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+       /* String operator;
+        Scanner in =new Scanner(System.in);
+        System.out.println("Enter an operator:");
+        operator=in.next();
+        if(operator.equals("+")||operator.equals("-")||operator.equals("*")||operator.equals("/")||operator.equals("%"))
+        {
+            System.out.println("it is an arithmetic operator");
+        }
+        else{
+                  System.out.println("it is not an arithmetic operator"); 
+                }*/
+       
+       
+       
+      /* int age;
+       Scanner in =new Scanner(System.in);
+       System.out.println("Enter Age:");
+       age=in.nextInt();
+       if(age>18){
+            System.out.println("Your are eligible to vote");
+        }
+       else{
+           System.out.println("Your are not eligible to vote");
+       }*/
+      
+      
+      
+     /* int num1;
+         int num2;
+      Scanner in=new Scanner(System.in);
+        System.out.println("Enter num1:");
+        num1=in.nextInt();
+        System.out.println("Enter num2:");
+        num2=in.nextInt();
+if(num1>num2){
+    System.out.println(num1+"is a greater number");
+}
+else{
+    System.out.println(num2+"is a greater number");
+}   */   
+     
+        
+     
+     /*int mark;
+     Scanner in =new Scanner(System.in);
+       System.out.println("Enter mark:");
+       mark=in.nextInt();
+       if(mark>=40){
+            System.out.println("Grade = Pass");
+        }
+       else{
+           System.out.println("Grade = Fail");
+       }  */
+     
+     
+   /*  String name;
+     String pwd;
+       Scanner in =new Scanner(System.in);
+       System.out.println("Enter Name:");
+       name=in.nextLine();
+       System.out.println("Enter Password:");
+       pwd=in.nextLine();
+       if(name .equals ("janaki") && pwd .equals ("1234")){
+            System.out.println("Welcome janaki");
+        }
+       else{
+           System.out.println(" match not found");
+       }*/
+     
+   
+   
+    }
+    
+}
